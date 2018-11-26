ba0088940227da1590680a2951483208d6225300
WaKuangGe <1134774872@qq.com>
2018-11-26 19:34:35 +0800

    优化

---
+++ Common.Core/CommonCore.Testing/EFCoreTesting.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonCore.Testing
+{
+    [TestClass]
+    public class EFCoreTesting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
